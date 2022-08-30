a05b51b5a27ca96d9d79d482347fbff7aba37088
Administrator <2815418919@qq.com>
2022-08-30 10:23:22 +0800

    dfdfg

--- contorller/src/main/java/com/example/contorller/test/Servlet.java
+++ contorller/src/main/java/com/example/contorller/test/Servlet.java
@@ -15,7 +15,7 @@ public class Servlet {
     @ResponseBody
     public String user(){
         User user = new User();
-        user.setAge("122,0");
+        user.setAge("122,ewfdfsdfs0");
         user.setNAME("GIT");
         return  user.toString();
     }
